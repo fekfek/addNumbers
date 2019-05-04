b7be98ccff9a1746e92672bf27353c3b4f42212c
fhmarian <fhmarian@yahoo.com>
2019-05-04 11:57:48 -0400

    Mukera2 class added

--- src/main/java/com/mukera/Mukera2.java
+++ src/main/java/com/mukera/Mukera2.java
@@ -1,9 +1,9 @@
 package com.mukera;
 
-public class Mukera {
+public class Mukera2 {
 	
 	public static void main(String[] args) {
-		System.out.println("This is for Mukera");
+		System.out.println("This is Mukera 2");
 	}
 
 }
